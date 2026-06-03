bfc2c72e5edc4d49681baf7a45c632eb33a50642
Farwa Zafar <pov.farwa@gmail.com>
2026-06-03 17:24:24 +0500

    sdded the code of oled screen if it not connected and also describe what the pinMode be

--- src/main.cpp
+++ src/main.cpp
@@ -12,3 +12,16 @@
 #define BUZZER_PIN 18
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1)
+
+void setup(){
+    Serial.begin(115200);
+    pinMode(VIBRATION_PIN, INPUT);
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(BUZZER_PIN, OUTPUT);
+
+    Wire.begn(SDA_PIN< SCL_PIN)
+
+    if(display.begin(SSD1306_SWITCHCAPVCC, 0x3C)){
+        Serial.println("OLED NOT FOUND")
+    }
+}
